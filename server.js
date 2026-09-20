84f3fe337d95bad3c7470c14bee66080a4a51337
nithin naidu <nathinnaidu2007@gmail.com>
2026-09-20 14:17:37 +0530

    Use MySQL connection pool

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require("path");
 const mysql = require("mysql2");
 
 // MySQL connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 db.connect((err) => {
 

87f9c239ac85c468a980b33eed588f65f3c405a7
nithin naidu <nathinnaidu2007@gmail.com>
2026-09-20 13:59:41 +0530

    Configure Railway MySQL port

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const mysql = require("mysql2");
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: "localhost",
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
